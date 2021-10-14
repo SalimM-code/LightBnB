39efae090d5ce7ad24da4d5f040c09263ae3dd4f
salimM-code <mustafasalim000@gmail.com>
2021-10-14 19:59:34 -0400

    added user reservations query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -80,7 +80,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const sqlQuery =`
+  SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN reservations ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date  < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2
+  `;
+  return pool 
+    .query(sqlQuery, [guest_id, limit])
+    .then(res => {
+      return res.rows
+    })
 }
 exports.getAllReservations = getAllReservations;
 
